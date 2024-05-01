c78cf1209925048f73cc0c5920f977cf6d90c85c
requiempt <145904266+requiempt@users.noreply.github.com>
2024-05-01 20:59:30 +0300

    Update init.js

--- js/init.js
+++ js/init.js
@@ -1,4 +1,20 @@
+$(function(){
 
+  var $w = $(window),
+      $background = $('#background');
+
+  // Fix background image jump on mobile
+  if ((/Android|iPhone|iPad|iPod|BlackBerry/i).test(navigator.userAgent || navigator.vendor || window.opera)) {
+    $background.css({'top': 'auto', 'bottom': 0});
+
+    $w.resize(sizeBackground);
+    sizeBackground();
+  }
+
+  function sizeBackground() {
+     $background.height(screen.height);
+  }
+});
 $(document).ready(function(){
   $('.slider').slick({
     slidesToShow: 1,
