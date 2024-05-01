0f06971e71e7c220f857b4dc5bfa08fcde3700f5
requiempt <145904266+requiempt@users.noreply.github.com>
2024-05-01 20:03:43 +0300

    Update init.js

--- js/init.js
+++ js/init.js
@@ -1,9 +1,4 @@
-const vh = window.innerHeight * 0.01;
-document.documentElement.style.setProperty('--vh', `${vh}px`);
-document.body.setAttribute(
-  'style',
-  `height: calc(var(--vh, 1vh) * 100);`
-);
+
 $(document).ready(function(){
   $('.slider').slick({
     slidesToShow: 1,
